97c428a030635b2c67bc33a307c36c1745cd00a2
Dylan Loke <dylanloke4896@gmail.com>
2024-01-22 19:42:50 +0800

    new page for php uses

---
+++ pricing_style.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="8-UTF">
+    <meta name = "Viewport" content=""width="device-width", initial scale="1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    <link rel="Stylesheet" href="css/pricing_style.css">
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+            <h2><b><font style="font-family:inter">USER PRICING PLAN</font></b></h2>
+            <div class ="price-row">
+                <div class = "price-col">
+                    <p>Standard</p>
+                    <h3><font size = "5">RM</font> 10.00<br><span>/month</span></h3>
+                    <ul>
+                        <li><i class="fa fa-check" style="color: darkgreen;"><font color="#ffffff">Ads Free</font></i></li>
+                        <li><i class="fa fa-check" style="color: darkgreen;"><font color="#ffffff">Unlmited Upload Spaces</font></i></li>
+                        <li><i class="fa fa-times" style="color: rgb(233, 0, 0);"><font color="#ffffff" >Free Usage</font></i></li>
+                        <li><i class="fa fa-times" style="color: rgb(233, 0, 0);"><font color="#ffffff" >Allow quality solution</font></i></li>
+                        <li><i class="fa fa-times" style="color: rgb(233, 0, 0);"><font color="#ffffff" >Offline Listening Plan</font></i></li>
+                        <div class = "button1"> 
+                            <button disabled><i class = "fa fa-check"></i></button>
+                        </div>
+                    </ul>
+                </div>
+                <div class = "price-col">
+                    <p>Premium</p>
+                    <h3><font size = "5">RM</font> 30.00<br><span>/month</span></h3>
+                    <ul>
+                        <li><i class="fa fa-check" ><font color="#ffffff">Ads Free</font></i></li>
+                        <li><i class="fa fa-check" ><font color="#ffffff">Unlmited Upload Spaces</font></i></li>
+                        <li><i class="fa fa-check" ><font color="#ffffff">Free Usage</font></i></li>
+                        <li><i class="fa fa-check" ><font color="#ffffff">Allow quality solution</font></i></li>
+                        <li><i class="fa fa-check" ><font color="#ffffff">Offline Listening Plan</font></i></li>
+                        <button>PURCHASE</button>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="8-UTF">
+        <meta name = "Viewport" content=""width="device-width", initial scale="1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+        <link rel="Stylesheet" href="css/register.css">
+    </head>
+    <body>
+        <div class ="container">
+            <div class = "form-box">
+                <h1><font color="#01B3FF">REGISTRATION</font></h1>
+                <div class = "sub-word">
+                    <h4><b><font style="color:#ffffff; text-align: center; font-weight: 300;"><br>First User? Don't worry, you can now create your own</font></b></h4>
+                </div>
+                    <div class = "input-box">
+                        <div class = "input-field">
+                            <label><input type ="text" 
+                                placeholder="Username">
+                            </label>
+                        </div>
+                        <div class = "input-field">
+                            <label><input type ="text" 
+                                placeholder="Email">
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class = "input-box">
+                        <div class = "input-field">
+                            <label><input type ="password" 
+                                placeholder="Password">
+                            </label>
+                        </div>
+                        <div class = "input-field">
+                            <label><input type ="password" 
+                                placeholder="Confirm Password">
+                            </label>
+                        </div>
+                    </div>
+                <label>
+                    <input type="checkbox">
+                        <b style="color: #ffffff;">I agree to terms and agreement and private policy</b>
+                </label>
+                <div class = "btn-class-style">
+                    <button type="button" class="btn-reg">
+                        <b>
+                            <font size = "1">
+                            BACK TO LOGIN
+                            </font>
+                        </b>
+                    </button>
+                    <button type="button" class="btn-reg">
+                        <b>
+                            <font size = "1">
+                            REGISTER
+                            </font>
+                        </b>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
